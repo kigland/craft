d6a9b6428b0396767f0b067eb718eea0921b142a
RemiIO Scarlet <i@remi.email>
2023-12-04 02:01:37 +0800

    feat: bind morphTargetInfluences with cube model (test)

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import { Canvas, } from '@react-three/fiber'
 import './App.css'
 import { OrbitControls, Text } from '@react-three/drei'
 import Face from './components/Face'
-import { Switch } from "@nextui-org/react";
-
+import CubeTester from './components/Cube'
+import { Switch, Slider } from "@nextui-org/react";
 function App() {
 
   const [count, setCount] = useState(0);
 
   return (
-    <div className="  h-screen flex flex-row justify-between items-center">
+    <div className="h-screen flex flex-row justify-between items-center">
       <div className="w-2/3 h-full ">
         <Canvas >
           <ambientLight />
@@ -24,9 +24,31 @@ function App() {
         </Canvas>
       </div>
       <div className="w-1/3 p-2 overflow-auto rounded-3xl shadow-2xl">
-
+        <div className="h-80">
+          <Canvas >
+            <ambientLight />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+            <CubeTester shapeKey={[count]} />
+            <OrbitControls />
+            {/* <Text position={[0, 3, 0]} scale={[0.5, 0.5, 0.5]}> craft.kig.land</Text> */}
+          </Canvas>
+        </div>
         <div>
-          <Switch defaultSelected>开启表面细分</Switch>
+          <Slider
+            label="Morph Target"
+            step={0.01}
+            maxValue={1}
+            minValue={0}
+            onChange={(value) => { setCount(value as number) }}
+            defaultValue={0}
+            className="max-w-md"
+          />
+        </div>
+
+        <>{count}</>
+        <div>
+          <Switch > subvision </Switch>
         </div>
       </div>
     </div>
