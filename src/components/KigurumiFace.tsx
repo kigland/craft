d7a855da5a59295d9eeb6385b6ef2ee96b558360
RemiIO Scarlet <i@remi.email>
2024-02-14 18:13:11 +0800

    feat: face component support DRACO & exposing morph target

--- src/components/KigurumiFace.tsx
+++ src/components/KigurumiFace.tsx
@@ -4,39 +4,55 @@ import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three-stdlib'
 import * as THREE from 'three'
 import { LoopSubdivision } from 'three-subdivide'
+import { DRACOLoader } from 'three-stdlib'
+
+export const loadDRACOModel = (loader: any) => {
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath('/draco/');
+  dracoLoader.preload();
+  loader.setDRACOLoader(dracoLoader);
+}
 
 const KigurumiFace = (props: ThreeElements['mesh'] & {
   shapeValues?: number[],
   faceModelUrl?: string,
   color?: string,
   subvision?: number,
+  setKigurumiMorphTargetDictionary?: (labels: string[] | any) => void,
 }) => {
 
   const {
     shapeValues = [0],
     faceModelUrl,
     subvision = 0,
+    setKigurumiMorphTargetDictionary,
   } = props;
 
   const ref = useRef<ThreeElements['primitive']>(null!);
-  const cubeGeometry: any = useLoader(GLTFLoader, faceModelUrl || '/cube.glb');
+
+  const cubeGeometry: any = useLoader(GLTFLoader, faceModelUrl || '/cube.glb',);
 
   useEffect(() => {
     ref.current.children[0].morphTargetInfluences = shapeValues;
     ref.current.children[0].material.color = new THREE.Color(props.color || "#f3c4bf");
-
   }, [shapeValues, props.color]);
 
   useEffect(() => {
     subvision > 0 && (ref.current.children[0].geometry = LoopSubdivision
-      .modify(cubeGeometry.scene.children[0].geometry, 1, {
+      .modify(cubeGeometry.scene.children[0].geometry, 2, {
         split: true,       // optional, default: true
         uvSmooth: true,      // optional, default: false
         preserveEdges: true,      // optional, default: false
-        flatOnly: false,      // optional, default: false
-        maxTriangles: 10000,   // optional, default: Infinity
+        flatOnly: true,      // optional, default: false
+        maxTriangles: Infinity,   // optional, default: Infinity
       }))
   }, [subvision]);
+
+  useEffect(() => {
+    // morphTargetDictionary to set labels
+    setKigurumiMorphTargetDictionary
+      && setKigurumiMorphTargetDictionary(cubeGeometry.scene.children[0].morphTargetDictionary);
+  }, []);
 
   return <mesh {...props}>
     {props.children}
